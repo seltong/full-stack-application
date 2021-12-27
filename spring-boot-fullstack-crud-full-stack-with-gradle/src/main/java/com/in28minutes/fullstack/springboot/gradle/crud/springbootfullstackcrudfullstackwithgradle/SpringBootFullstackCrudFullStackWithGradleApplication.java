8800ced4555eb7508930820825796f65c3a1617d
Selton Guedes <sfgs@ecomp.poli.br>
2021-12-27 13:06:35 -0300

    Add new backend folders

---
+++ spring-boot-fullstack-crud-full-stack-with-gradle/src/main/java/com/in28minutes/fullstack/springboot/gradle/crud/springbootfullstackcrudfullstackwithgradle/SpringBootFullstackCrudFullStackWithGradleApplication.java
@@ -0,0 +1,13 @@
+package com.in28minutes.fullstack.springboot.gradle.crud.springbootfullstackcrudfullstackwithgradle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootFullstackCrudFullStackWithGradleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootFullstackCrudFullStackWithGradleApplication.class, args);
+	}
+
+}
---
+++ spring-boot-fullstack-crud-full-stack-with-gradle/src/test/java/com/in28minutes/fullstack/springboot/gradle/crud/springbootfullstackcrudfullstackwithgradle/SpringBootFullstackCrudFullStackWithGradleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.in28minutes.fullstack.springboot.gradle.crud.springbootfullstackcrudfullstackwithgradle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootFullstackCrudFullStackWithGradleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
