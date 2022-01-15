4624f855dbcf72f8a8da5a648d60316b25969cd1
Selton Guedes <sfgs@ecomp.poli.br>
2022-01-15 11:41:18 -0300

    Step-3 Enhancing React App to consume the REST API

---
+++ frontend-spring-boot-react-crud-full-stack-with-maven/src/component/ListCoursesComponent.jsx
@@ -0,0 +1,28 @@
+class ListCoursesComponent extends Component {
+
+  render() {
+      return (
+          <div className="container">
+              <h3>All Courses</h3>
+              <div className="container">
+                  <table className="table">
+                      <thead>
+                          <tr>
+                              <th>Id</th>
+                              <th>Description</th>
+                          </tr>
+                      </thead>
+                      <tbody>
+                          <tr>
+                              <td>1</td>
+                              <td>Learn Full stack with Spring Boot and Angular</td>
+                          </tr>
+                      </tbody>
+                  </table>
+              </div>
+          </div>
+      )
+  }
+}
+
+export default ListCoursesComponent
