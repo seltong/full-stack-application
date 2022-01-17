c9d853d764dbd6e8fcde1730f14855167955026c
Selton Guedes <sfgs@ecomp.poli.br>
2022-01-17 09:29:37 -0300

    Step-4 Enhancing React app with Delete Course Feature

--- frontend-spring-boot-react-crud-full-stack-with-maven/src/component/ListCoursesComponent.jsx
+++ frontend-spring-boot-react-crud-full-stack-with-maven/src/component/ListCoursesComponent.jsx
@@ -2,57 +2,71 @@ import React, { Component } from 'react';
 import CourseDataService, { INSTRUCTOR } from '../service/CourseDataService';
 class ListCoursesComponent extends Component {
 
-  constructor(props) {
-    super(props)
-    this.state = {
-        courses: [],
-        message: null
+    constructor(props) {
+        super(props)
+        this.state = {
+            courses: [],
+            message: null
+        }
+        this.refreshCourses = this.refreshCourses.bind(this)
     }
-    this.refreshCourses = this.refreshCourses.bind(this)
-}
 
-componentDidMount() {
-    this.refreshCourses();
-}
+    componentDidMount() {
+        this.refreshCourses();
+    }
 
-refreshCourses() {
-    CourseDataService.retrieveAllCourses(INSTRUCTOR)//HARDCODED
-        .then(
-            response => {
-                console.log(response);
-                this.setState({ courses: response.data })
-            }
-        )
-}
+    refreshCourses() {
+        CourseDataService.retrieveAllCourses(INSTRUCTOR)//HARDCODED
+            .then(
+                response => {
+                    console.log(response);
+                    this.setState({ courses: response.data })
+                }
+            )
+    }
 
-  render() {
-      return (
-        <div className="container">
-            <h3>All Courses</h3>
+    deleteCourseClicked(id) {
+        CourseDataService.deleteCourse(INSTRUCTOR, id)
+            .then(
+                response => {
+                    this.setState({ message: `Delete of course ${id} Successful` })
+                    this.refreshCourses()
+                }
+            )
+    
+    }
+
+    render() {
+        return (
             <div className="container">
-                <table className="table">
-                    <thead>
-                        <tr>
-                            <th>Id</th>
-                            <th>Description</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        {
-                            this.state.courses.map(
-                                course =>
-                                    <tr key={course.id}>
-                                        <td>{course.id}</td>
-                                        <td>{course.description}</td>
-                                    </tr>
-                            )
-                        }
-                    </tbody>
-                </table>
+                <h3>All Courses</h3>
+                {this.state.message && <div class="alert alert-success">{this.state.message}</div>}
+                
+                <div className="container">
+                    <table className="table">
+                        <thead>
+                            <tr>
+                                <th>Id</th>
+                                <th>Description</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {
+                                this.state.courses.map(
+                                    course =>
+                                        <tr key={course.id}>
+                                            <td>{course.id}</td>
+                                            <td>{course.description}</td>
+                                            <td><button className="btn btn-warning" onClick={() => this.deleteCourseClicked(course.id)}>Delete</button></td>
+                                        </tr>
+                                )
+                            }
+                        </tbody>
+                    </table>
+                </div>
             </div>
-        </div>
-      )
-  }
+        )
+    }
 }
 
 export default ListCoursesComponent
--- frontend-spring-boot-react-crud-full-stack-with-maven/src/service/CourseDataService.js
+++ frontend-spring-boot-react-crud-full-stack-with-maven/src/service/CourseDataService.js
@@ -9,6 +9,11 @@ class CourseDataService {
     retrieveAllCourses(name) {
         return axios.get(`${INSTRUCTOR_API_URL}/courses`);
     }
+
+    deleteCourse(name, id) {
+        //console.log('executed service')
+        return axios.delete(`${INSTRUCTOR_API_URL}/courses/${id}`);
+    }
 }
 
 export default new CourseDataService();
