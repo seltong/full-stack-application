c80ed39d1b2c3c45e5d8a31e0f13954c97b7bcc6
Selton Guedes <sfgs@ecomp.poli.br>
2022-01-17 11:28:48 -0300

    Step-4 Invoking the API from Course Component

--- frontend-spring-boot-react-crud-full-stack-with-maven/src/component/CourseComponent.jsx
+++ frontend-spring-boot-react-crud-full-stack-with-maven/src/component/CourseComponent.jsx
@@ -1,9 +1,46 @@
 import React, { Component } from 'react';
+import { matchPath } from 'react-router';
+import CourseDataService, { INSTRUCTOR } from '../service/CourseDataService';
+import { createBrowserHistory } from 'history';
 
 class CourseComponent extends Component {
+
+  constructor(props) {
+    super(props);
+
+    let history = createBrowserHistory();
+    let match = matchPath({
+      path: "/courses/:id",
+      exact: true
+    }, history.location.pathname);
+
+    this.state = {
+      id: match.params.id,
+      description: ''
+    }
+  }
+
+  componentDidMount() {
+    // eslint-disable-next-line
+    if (this.state.id == -1) {
+      return
+    }
+
+    CourseDataService.retrieveCourse(INSTRUCTOR, this.state.id)
+      .then(response => this.setState({
+        description: response.data.description
+      }));
+  }
+
   render() {
+    let { description, id} = this.state
+
     return (
-      <h1>Course Details</h1>
+      <div>
+          <h3>Course</h3>
+          <div>{id}</div>
+          <div>{description}</div>
+      </div>
     );
   }
 }
